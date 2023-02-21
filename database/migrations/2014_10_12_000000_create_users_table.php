91dd90a397e0866ed7ffbce53276d2d0b5a2075a
redeye0077 <red234013@gmail.com>
2023-02-21 22:52:03 +0900

    ログイン時メールアドレスではなくユーザー名で認証する機能の実装

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 60);
-            $table->string('name', 50);
+            $table->string('name', 50)->unique();
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,5 +4,5 @@
 <a href="/share" class="btn share">共有</a>
 <a href="/calendar" class="btn calendar">学習時間の表示</a>
 <a href="/study_challenges" class="btn study_challenges">目標</a>
-<a href="/setting" class="btn settings">設定</a>
+<a href="/settings" class="btn settings">設定</a>
 <a href="#" class="btn logout">ログアウトする</a>
