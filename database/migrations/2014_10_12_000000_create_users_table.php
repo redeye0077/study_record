5f055befb34e2ef191a9d3f42566dea0dfa667d0
redeye0077 <red234013@gmail.com>
2023-02-26 14:30:36 +0900

    退会機能の実装

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('password', 60);
             $table->string('name', 50)->unique();
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'テスト',
+            'email' => 'test@example.com',
+            'password' => 'sasasasasas',
+        ]);
+    }
+}
