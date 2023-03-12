3d18390d949564f3ca9c9309bcd4935759788db8
redeye0077 <red234013@gmail.com>
2023-03-12 22:45:24 +0900

    最後に更新されたレコードを取得するようにした

--- app/Http/Controllers/StudyChallengesController.php
+++ app/Http/Controllers/StudyChallengesController.php
@@ -11,7 +11,10 @@ class StudyChallengesController extends Controller
     public function index()
     {   
         //monthly_goal_tableに保存されているtarget_hourとtarget_minutesを取得する
-        $monthlyGoal = MonthlyGoal::where('month', Carbon::now()->format('Y-m') . '-01')->first();
+        $monthlyGoal = MonthlyGoal::latest('updated_at')
+        ->where('month', Carbon::now()->format('Y-m') . '-01')
+        ->first(['target_hour', 'target_minutes']);
+        
         return view('study_challenges', compact('monthlyGoal'));
     }
 }
