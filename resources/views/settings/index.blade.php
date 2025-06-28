07dd2f41db622429044d565a39267f9c0557dfda
redeye0077 <red234013@gmail.com>
2025-06-28 14:24:12 +0900

    設定画面のCSSを分離

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -1,53 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>設定画面</title>
-    <style>
-        body {
-            background-color: #f5f5f5;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            margin: 0;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-        }
-        h1 {
-            text-align: center;
-            margin-top: 0;
-            margin-bottom: 1rem;
-            font-size: 3rem;
-        }
-        .button-container {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            margin-top: 1rem;
-        }
-        button {
-            width: 15rem;
-            height: 2.5rem;
-            margin: 0.5rem;
-        }
+@extends('layouts.app')
 
-        /* メディアクエリ */
-        @media (max-width: 640px) {
-            .button-container {
-                flex-direction: column;
-                align-items: center;
-            }
-            button {
-                margin-top: 0.5rem;
-            }
-        }
-    </style>
-</head>
-<body>
+@section('title', '設定画面')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/settings/index.css') }}">
+@endsection
+
+@section('content')
+<div class="body">
     <h1>設定</h1>
     <div class="button-container">
         <button type="button" class="btn-blue" onclick="location.href='/settings/login_id';">
@@ -63,5 +23,5 @@
             退会する
         </button>
     </div>
-</body>
-</html>
+</div>
+@endsection
